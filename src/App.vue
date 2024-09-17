21e3a4220dd054b3dc71a50049e09e870d8befbd
wilsonRueda <wilson.rueda@onegroupitservices.com>
2024-09-17 00:59:44 -0500

    init proyect

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Header from "../src/components/Header/Header.vue";
+components: {
+  Header
+}
+</script>
+
+<template>
+  <div class="app">
+    <Header />
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,94 @@
+<script setup>
+</script>
+
+<template>
+    <div class="header">
+        <div class="header__logo">
+            <p class="header__name">Softinkra</p>
+            <p class="header__main">Innovando el futuro</p>
+            <p class="header__second">Un código a la vez</p>
+        </div>
+        <div class="header__menu">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+            </svg>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+
+    &__name {
+        font-family: 'Ubuntu', sans-serif;
+        font-weight: 800;
+        margin: 0;
+    }
+
+    &__main,
+    &__second {
+        margin: 0;
+        display: inline-block;
+        opacity: 0;
+        position: absolute;
+        font-family: 'Ubuntu', sans-serif;
+    }
+
+    &__main {
+        animation: fadeInOutMain 4s forwards;
+    }
+
+    &__second {
+        animation: fadeInSecond 4s forwards 2s;
+    }
+
+    &__menu {
+        width: 40px;
+        height: 40px;
+        cursor: pointer;
+    }
+}
+
+
+@keyframes fadeInOutMain {
+    0% {
+        opacity: 0;
+        transform: translateY(-10px);
+    }
+
+    25% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+
+    75% {
+        opacity: 1;
+        transform: translateY(0);
+    }
+
+    100% {
+        opacity: 0;
+        transform: translateY(-10px);
+    }
+}
+
+@keyframes fadeInSecond {
+    0% {
+        opacity: 0;
+        transform: translateX(-10px);
+    }
+
+    50% {
+        opacity: 0;
+        transform: translateX(0);
+    }
+
+    100% {
+        opacity: 1;
+        transform: translateX(0);
+    }
+}
+</style>
